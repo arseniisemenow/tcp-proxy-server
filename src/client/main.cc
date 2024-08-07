eaead907f6de42ce9b667e29aa126de19ac5de34
Evangelina Marj <evangelm@va-h5.kzn.21-school.ru>
2024-08-08 00:51:43 +0300

    main. Refactoring. Make log messager prettier

--- src/client/main.cc
+++ src/client/main.cc
@@ -5,33 +5,33 @@
 #include <unistd.h>
 
 void SendSqlQuery(const short port, const char *address, const std::string &query) {
-  int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (client_socket < 0) {
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket < 0) {
     perror("Socket creation failed");
     exit(EXIT_FAILURE);
   }
 
-  struct sockaddr_in server_addr{};
+  struct sockaddr_in server_addr {};
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(address);
 
-  if (connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+  if (connect(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
     perror("Connect failed");
-    close(client_socket);
+    close(server_socket);
     exit(EXIT_FAILURE);
   }
 
-  send(client_socket, query.c_str(), query.size(), 0);
+  send(server_socket, query.c_str(), query.size(), 0);
   char buffer[1024] = {0};
-  ssize_t bytes_read = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+  ssize_t bytes_read = recv(server_socket, buffer, sizeof(buffer) - 1, 0);
   if (bytes_read > 0) {
     buffer[bytes_read] = '\0';
     std::cout << "Server response: " << buffer << std::endl;
   }
 
-  close(client_socket);
+  close(server_socket);
 }
 
 int main() {
@@ -39,25 +39,25 @@ int main() {
   const char *server_address = "127.0.0.1";
 
   const char *drop_table_sql = "DROP TABLE IF EXISTS users;";
-  const char *create_table_sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,age INTEGER NOT NULL);";
+  const char *create_table_sql =
+      R"("CREATE TABLE IF NOT EXISTS users
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        age INTEGER NOT NULL);")";
   const char *insert_data_sql = R"(
-        INSERT INTO users (name, age) VALUES ('Alice', 30);
-        INSERT INTO users (name, age) VALUES ('Bob', 25);
-        INSERT INTO users (name, age) VALUES ('Charlie', 35);
+        INSERT INTO users (name, age) VALUES ('Oliver', 25);
+        INSERT INTO users (name, age) VALUES ('Wladimir', 30);
+        INSERT INTO users (name, age) VALUES ('Kris', 35);
     )";
   const char *select_data_sql = R"(SELECT * FROM users;)";
-//  SendSqlQuery(server_port, server_address, drop_table_sql);
+  const char *select_sqlite_version_sql = R"(SELECT sqlite_version();)";
+  //  SendSqlQuery(server_port, server_address, drop_table_sql);
   SendSqlQuery(server_port, server_address, select_data_sql);
   SendSqlQuery(server_port, server_address, create_table_sql);
   SendSqlQuery(server_port, server_address, select_data_sql);
   SendSqlQuery(server_port, server_address, insert_data_sql);
   SendSqlQuery(server_port, server_address, select_data_sql);
 
-  SendSqlQuery(server_port, server_address, "SELECT sqlite_version();");
-  SendSqlQuery(server_port, server_address, "PRAGMA database_list;");
-  SendSqlQuery(server_port, server_address, "PRAGMA page_size;");
-  SendSqlQuery(server_port, server_address, "PRAGMA encoding;");
-  SendSqlQuery(server_port, server_address, "PRAGMA compile_options;");
-  SendSqlQuery(server_port, server_address, "PRAGMA synchronous;");
+  SendSqlQuery(server_port, server_address, select_sqlite_version_sql);
   return 0;
 }
--- src/proxy-server/logger/logger.cc
+++ src/proxy-server/logger/logger.cc
@@ -1,13 +1,34 @@
 #include "logger.h"
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <sstream>
+
+std::tm GetCurrentTime() {
+  auto now = std::chrono::system_clock::now();
+  auto time = std::chrono::system_clock::to_time_t(now);
+  std::tm tm = *std::localtime(&time);
+  return tm;
+}
 
 std::string Logger::log(const std::string &message) {
-  std::ofstream logFile("sql_queries.log", std::ios_base::app);
-  if (logFile.is_open()) {
-    logFile << message << std::endl;
+  try {
+    std::ofstream log_file("sql_queries.log", std::ios_base::app);
+
+    if (!log_file.is_open()) {
+      throw std::ios_base::failure("Failed to open log file");
+    }
+
+    auto tm = GetCurrentTime();
+
+    std::ostringstream timestamp_stream;
+    timestamp_stream << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+
+    log_file << "[" << timestamp_stream.str() << "] " << message << std::endl;
+
     return message;
+  } catch (const std::ios_base::failure &e) {
+    std::cerr << "Logging error: " << e.what() << std::endl;
+    return "";
   }
-  logFile.close();
-  return "";
 }
